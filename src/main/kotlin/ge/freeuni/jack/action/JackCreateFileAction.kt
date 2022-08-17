b19fc3c62aacbe1b72ca255783db15fe71bcfc6c
Levan Apakidze <lapak18@freeuni.edu.ge>
2022-08-17 12:03:06 +0400

    added brace matcher and new jack file functionality

---
+++ src/main/kotlin/ge/freeuni/jack/action/JackCreateFileAction.kt
@@ -0,0 +1,19 @@
+package ge.freeuni.jack.action
+
+import com.intellij.ide.actions.CreateFileFromTemplateAction
+import com.intellij.ide.actions.CreateFileFromTemplateDialog
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiDirectory
+import ge.freeuni.jack.language.JackIcons
+
+class JackCreateFileAction: CreateFileFromTemplateAction(ACTION_NAME, "Creates new class", JackIcons.FILE) {
+    override fun buildDialog(project: Project, directory: PsiDirectory, builder: CreateFileFromTemplateDialog.Builder) {
+        builder.setTitle(ACTION_NAME).addKind("Class", JackIcons.FILE, ACTION_NAME)
+    }
+
+    override fun getActionName(directory: PsiDirectory?, newName: String, templateName: String?): String = ACTION_NAME
+    
+    companion object {
+        private const val ACTION_NAME="Jack Class"
+    }
+}
